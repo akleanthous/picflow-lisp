f5af92cfe74956549575ef4e5a29ccb7a59e3f56
sketerpot <sketerpot@4ce91c86-501a-0410-9175-750115596225>
2006-08-07 22:49:58 +0000

    Added integral block and a test for it. Also fixed wonky-counter-block in test.lisp, which was misbehaving.

---
+++ trunk/integral.lisp
@@ -0,0 +1,15 @@
+;; Various types of integral blocks
+
+(in-package :picflow)
+
+(defun integral-rectangle (type-specifier &optional c)
+  "Returns an integral with respect to time block, which takes and
+passes on a numeric argument of a type specified by the type-specifier
+string. Uses rectangular integration method, which can sometimes be
+the best, such as when it is applied to the output of the derivative
+block. Adds c (defaults to zero) to its output"
+  (c-block (format nil "void BLOCKNAME(~A arg) {
+  static ~:*~A value = 0;
+  value += arg;
+  $DEFAULT(value~:[~; + (~:*~A)~]);
+}" type-specifier c)))
