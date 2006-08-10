71ef1d801ca0b27ba05492cd85a0d71ba0b4bee0
sketerpot <sketerpot@4ce91c86-501a-0410-9175-750115596225>
2006-08-10 22:13:22 +0000

    Big overhaul of input linkage system. Inputs are now gensym'd, with a layer of indirection between their names and their C symbol names.

--- c-blocks.lisp
+++ c-blocks.lisp
@@ -15,20 +15,22 @@
 ;; name is default, and this is not case-sensitive (so you can spell
 ;; it DEFAULT or DeFaULt).
 ;; 
-;; To read an input, use the syntax "$[inputname]", where inputname is
-;; the (also case-sensitive) name of the input. The variable type
-;; defaults to int, but you can specify the type by referring to the
-;; input variable with the syntax "$[inputname]{type}" the first time
-;; it is used. If multiple conflicting type declarations are given,
-;; all but the first are quietly ignored. If the type is not declared
-;; the first time an input variable is used, any subsequent type
-;; declarations will be ignored!
+;; To read an input, use the syntax "$[INPUTNAME]", where INPUTNAME is
+;; the (also case-sensitive) name of the input. It should be
+;; uppercase, but this is not mandatory. The variable type defaults to
+;; int, but you can specify the type and default value by referring to
+;; the input variable with the syntax
+;; "$[INPUTNAME]{type}{default-value}" the first time it is used. If
+;; multiple conflicting type declarations are given, all but the first
+;; are quietly ignored. If the type is not declared the first time an
+;; input variable is used, any subsequent type declarations will be
+;; ignored!
 ;; 
 ;; // Example proto-C code
 ;; void BLOCKNAME(int input) {
-;;   printf("Custom C block called! Flower number: %i\n", $[flowernumber]{unsigned int});
+;;   printf("Custom C block called! Flower number: %i\n", $[FLOWERNUMBER]{unsigned int}{0});
 ;;   $answer(42);
-;;   $default($[flowernumber]);
+;;   $default($[FLOWERNUMBER]);
 ;; }
 
 (defclass c-block-node (node)
@@ -38,18 +40,30 @@
    (text :reader c-block-text
 	 :initarg :text
 	 :documentation "The unprocessed text of the C expansion.")
-   (inputs :accessor c-block-inputs
-	   :initarg :inputs
-	   :documentation "The input names and types as a list of
-(name . type) conses, where the names are non-upcased keyword symbols
-and the types are C type specifier strings.")))
+   (c-block-inputs :accessor c-block-inputs
+		   :initarg :c-block-inputs
+		   :documentation "The input names and types as a list
+of (name type  default-value) lists, where the names are non-upcased keyword
+symbols and the types and default values are C type specifier strings.")))
 
-(defun c-block (text)
-  (let ((node (make-instance 'c-block-node :text text))
+(defun c-block (text &key (block-name "cBlock"))
+  "Return a C block given the proto-C TEXT. You may specify a block
+name, from which the real block name will be generated. The block name
+defaults to 'cBlock'."
+  (let ((node (make-instance 'c-block-node :text text :entry-point (c-gensym block-name)))
 	(inputs (extract-inputs text)))
-    (setf (output-names node)    (extract-outputs text))
-    (setf (input-variables node) (cons :default (mapcar #'car inputs)))
+    ;; Extract the output names and set them up
+    (setf (output-names node) (extract-outputs text))
+    ;; Extract the input names and set them up
+    (setf (input-names node) (cons :default (mapcar #'first inputs)))
+    ;; Generate a bunch of gensyms for the inputs and set up the
+    ;; name-to-gensymmed-name mapping.
+    (dolist (input-name (mapcar #'first inputs))
+      (setf (gethash input-name (inputs node)) 
+	    (c-gensym (symbol-name input-name))))
+    ;; Save the inputs data structure for later
     (setf (c-block-inputs node)  inputs)
+    ;; Return the node itself for use by user code
     node))
 
 (defun extract-outputs (text)
@@ -61,6 +75,11 @@ performed."
 			     (cl-ppcre:all-matches-as-strings "\\$[a-zA-Z_]+(?=\\s*\\()" text))
 		     :test #'equalp :from-end t))
 
+;; This function is needed for some of the proto-C parsing. Most of
+;; the "parsing" is done with regular expressions, but regexps are
+;; notoriously poor at matching balanced parenthetical
+;; expressions. This plugin to cl-ppcre adds matching ability for this
+;; specific thing.
 (defun matched-parens-filter (pos)
   "Match a region of text enclosed by balanced parentheses"
   (when (< pos cl-ppcre::*end-pos*)
@@ -107,39 +126,52 @@ performed."
 
 (defun extract-inputs (text)
   "Given a block of unprocessed proto-C, extract the input names and
-types and return them as a list of (name . type) conses, where the
-names are non-upcased symbols in the KEYWORD package and the types are
-C type specifier strings."
+types and return them as a list of (name type default-value) lists,
+where the names are non-upcased symbols in the KEYWORD package and the
+types are C type specifier strings. You're encouraged to use uppercase
+input names, but it's not strictly necessary."
   (let ((uses (collecting
-		(cl-ppcre:do-matches-as-strings (match "\\$\\[([a-zA-Z_]+)\\](\\{([^}]*)\\})?" text)
-		  (cl-ppcre:register-groups-bind (name worthless-gibberish type)
-		      ("\\$\\[([a-zA-Z_]+)\\](\\{([^}]*)\\})?" match)
-		    (declare (ignore worthless-gibberish))
-		    (collect (cons (intern name "KEYWORD") (or type "int"))))))))
+		(cl-ppcre:do-matches-as-strings (match "\\$\\[([a-zA-Z_]+)\\](\\{([^}]*)\\})?(\\{([^}]*)\\})?" text)
+		  (cl-ppcre:register-groups-bind (name worthless-gibberish type more-worthless-gibberish default-value)
+		      ("\\$\\[([a-zA-Z_]+)\\](\\{([^}]*)\\})?(\\{([^}]*)\\})?" match)
+		    (declare (ignore worthless-gibberish more-worthless-gibberish))
+		    (collect (list (intern name "KEYWORD") (or type "int") (or default-value "0"))))))))
     (remove-duplicates uses :test #'equalp :key #'car :from-end t)))
 
-(defun replace-inputs (text)
+(defun replace-inputs (text node)
   "Given a block of unprocessed proto-C, replace the input variables
 with code actually referencing the variables"
-  (cl-ppcre:regex-replace-all "\\$\\[([a-zA-Z_]+)\\](\\{([^}]*)\\})?" text "\\1"))
+  (cl-ppcre:regex-replace-all "\\$\\[([a-zA-Z_]+)\\](\\{([^}]*)\\})?(\\{([^}]*)\\})?" text 
+			      #'(lambda (match input-name &rest worthless-gibberish)
+				  (declare (ignore match worthless-gibberish))
+				  (lookup-input-variable-name node (intern input-name "KEYWORD")))
+			      :simple-calls t))
 
+;; This is some sample code with no purpose other than parser testing.
 (defparameter *sample-code* "void BLOCKNAME(int input) {
-  printf(\"Custom C block called! Flower number: %i\n\", $[flowernumber]{unsigned int});
-  $answer(42); $steveThePirate($[fool_input]);
-  $default($[flowernumber]);
+  printf(\"Custom C block called! Flower number: %i\n\", $[FLOWERNUMBER]{unsigned int}{1});
+  $answer(42); $steveThePirate($[FOOL_INPUT]);
+  $default($[FLOWERNUMBER]);
 }")
 
 ;; Standard node API methods
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defmethod prototype-code-emit ((node c-block-node))
-  (emit "// Declarations for ~S" node)
-  (loop for (input-name . input-type) in (c-block-inputs node)
-	do (emit "~A ~A;" input-type input-name)))
+  ;; Note: this doesn't make prototypes for functions defined in the
+  ;; proto-C. This should perhaps be changed, but MCC18 doesn't
+  ;; complain. Yet.
+  (loop for (input-name input-type default-value) in (c-block-inputs node)
+	do (emit "~A ~A = ~A;" 
+		 input-type 
+		 (lookup-input-variable-name node input-name)
+		 default-value)))
 
 (defmethod extra-code-emit ((node c-block-node))
+  ;; The whole pipeline: process the proto-C when necessary.
   (emit (replace-outputs (replace-inputs (replace-blockname (c-block-text node)
-							    (entry-point node)))
+							    (entry-point node))
+					 node)
 			 node)))
 
 #+nil
