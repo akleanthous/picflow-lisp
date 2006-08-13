4b9dc6cada29a324aac1432be69f125aa675835e
sketerpot <sketerpot@4ce91c86-501a-0410-9175-750115596225>
2006-08-13 20:43:53 +0000

    Further improvements and macroification

--- scratch/picflow_adc.h
+++ scratch/picflow_adc.h
@@ -23,4 +23,21 @@
 // A macro to glue together two bytes into a 10-bit unsigned integer
 #define TENBITS(H,L) ((((unsigned int)H) << 8) | L)
 
+// This macro should be called once in main() to set up sensible
+// default values for ADCON1 and ADCON2.
+#define setup_common_adc() {              \
+  ADCON1 = ADCON1_AN0_THROUGH_AN7_ANALOG; \
+  ADCON2 = ADCON2_DUMB_DEFAULT_VALUE;     \
+}
+
+// These macros change the analog channel.
+#define setADC0() { ADCON0 = ADCON0_BASE | ADCON0_AN0; }
+#define setADC1() { ADCON0 = ADCON0_BASE | ADCON0_AN1; }
+#define setADC2() { ADCON0 = ADCON0_BASE | ADCON0_AN2; }
+#define setADC3() { ADCON0 = ADCON0_BASE | ADCON0_AN3; }
+#define setADC4() { ADCON0 = ADCON0_BASE | ADCON0_AN4; }
+#define setADC5() { ADCON0 = ADCON0_BASE | ADCON0_AN5; }
+#define setADC6() { ADCON0 = ADCON0_BASE | ADCON0_AN6; }
+#define setADC7() { ADCON0 = ADCON0_BASE | ADCON0_AN7; }
+
 #endif
